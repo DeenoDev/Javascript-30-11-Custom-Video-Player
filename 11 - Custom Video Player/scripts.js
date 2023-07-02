603f9770effd3d70c1b114d4addc5e44170cfc22
DeenoDev <deyanoklaasen@outlook.com>
2023-07-02 16:43:25 +0200

    Created skip function that rewinds and skips video.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -23,9 +23,15 @@ function updateButton(){
     console.log('Update the button');
 }
 
+function skip(){
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
 //Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
