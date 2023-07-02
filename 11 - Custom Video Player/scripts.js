f225fbb1f52da6e9acd84a97d9d597bafbeb7b97
DeenoDev <deyanoklaasen@outlook.com>
2023-07-02 16:13:18 +0200

    Created togglePlay function to pause and play video.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -9,5 +9,12 @@ const ranges = player.querySelectorAll('.player__slider');
 
 
 //Build out functions
+function togglePlay(){
+    if(video.paused){
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 
 //Hook up the event listeners
