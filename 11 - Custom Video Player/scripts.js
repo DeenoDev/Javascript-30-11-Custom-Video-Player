bf2fa15e68aeb50c758cd218874fecc0869ddd05
DeenoDev <deyanoklaasen@outlook.com>
2023-07-02 16:31:47 +0200

    Created updateButton function that toggles video player icon when paused or playing.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -17,5 +17,15 @@ function togglePlay(){
    }
 }
 
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+    console.log('Update the button');
+}
+
 //Hook up the event listeners
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
